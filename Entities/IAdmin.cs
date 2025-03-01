bd362f146a778a105b4abb0f6df7f7bb49bbec3a
RoyzRonixx <124342183+RoyceHGill@users.noreply.github.com>
2025-03-01 23:16:57 +1000

    fixed algorithm to be more in line with appointment selection algorithm more accurate.

---
+++ Entities/IAdmin.cs
@@ -0,0 +1,19 @@
+﻿
+using JricaStudioWebAPI.Entities.Helpers;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace JricaStudioWebAPI.Entities
+{
+    public interface IAdmin : IBaseModel
+    {
+        Guid AdminKey { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string Password { get; set; }
+        string Phone { get; set; }
+        Guid ResetKey { get; set; }
+        DateTime ResetKeySent { get; set; }
+        string Username { get; set; }
+    }
+}
